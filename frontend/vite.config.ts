62667d8ccf9f9c7c5a057bbbbf4d7ec8629bb876
Konrad Sypek <konrad.sypek@o2.pl>
2025-02-20 12:01:39 +0100

    jest to vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config'; // ✅ Import from vitest/config
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    reporters: ['verbose'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [],
+    }
   },
 });
