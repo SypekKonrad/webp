d1c5afdec339331e81d0bd5125b9bb5b4edbade7
Konrad Sypek <konrad.sypek@o2.pl>
2025-02-20 10:27:11 +0100

    cra to vite

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(
@@ -23,5 +22,4 @@ root.render(
 
 
 
-reportWebVitals();
 
