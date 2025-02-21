92029a0310913418886fdde0e9a65062237fbf78
Konrad Sypek <konrad.sypek@o2.pl>
2025-02-21 01:42:29 +0100

    app.tsx test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import {MemoryRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+  render(
+      <MemoryRouter>
+          <App />
+      </MemoryRouter>
+
+  );
+  const navbar = screen.getByText(/Home/i);
+  const div = screen.getByText(/Konrad Sypek/i);
+  expect(navbar).toBeInTheDocument();
+  expect(div).toBeInTheDocument();
 });
