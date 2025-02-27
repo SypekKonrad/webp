a8300b1aca64e64e139ed0e53b395a70148b4a2b
Konrad Sypek <konrad.sypek@o2.pl>
2025-02-27 01:27:33 +0100

    settings.py debug fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = bool(os.environ.get("DEBUG", default=0))
+# DEBUG = bool(os.environ.get("DEBUG", default=0))
+DEBUG = os.environ.get("DEBUG", "False").lower() in ["1", "true"]
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", " ").split()
 
