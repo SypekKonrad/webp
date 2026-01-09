d5ab073249788dc57f84856066e9be8b56584b0f
Konrad Sypek <konrad.sypek@o2.pl>
2026-01-10 00:36:41 +0100

    vite config fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'; // ✅ Import from vitest/config
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
